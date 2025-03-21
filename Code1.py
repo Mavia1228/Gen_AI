d5a257c325e3636385fd79d8cc86c4d38b19b615
Mavia1228 <mavia1228@gmail.com>
2025-03-21 10:35:33 +0500

    list comprihension

--- Code1.py
+++ Code1.py
@@ -1,21 +1,32 @@
-ls = ['laHOre','kaRAchi','mulTan','bahaWALpur','pisHAwar']
-ls1 = []
-for x in ls:
-    x = x[0:1].upper() + x[1:-1].lower() + x[-1:].upper()
-    ls1.insert(0,x)
-ls = ls1
-del ls1
-print(ls)
+ls = ['laHOre','kaRAchi','mulTan','bahaWALpur','pisHAwar','siBi']
+# for x in range(len(ls)):
+#     print(ls[x])
+ls2 = [x.capitalize() for x in ls]
+print(ls2)
 
 
 
-#ls[0::2] = ["Sahiwal","bahalwapur"]
-#ls = ['lahore','karachi','multan','bahawalpur','pishawar']
+
+# ls = ['laHOre','kaRAchi','mulTan','bahaWALpur','pisHAwar']
 # ls1 = []
 # for x in ls:
-#     s = ls.pop()
-#     s = s[0:1].upper() + s[1:-1] + s[-1:].upper()
-#     s = s[0:1] + s[1:2].upper() + s[2:-2] + s[-2:-1].upper() + s[-1:]
-#     ls.insert(0,s)
+#     x = x[0:1].upper() + x[1:-1].lower() + x[-1:].upper()
+#     ls1.insert(0,x)
 # ls = ls1
+# del ls1
 # print(ls)
+
+#ls[0::2] = ["Sahiwal","bahalwapur"]
+#s = s[0:1].upper() + s[1:-1] + s[-1:].upper()
+
+#ls = [x for x in range(10)]
+
+# while x < len(ls):
+#     print(ls[x])
+#     x = x+1
+
+#list Comprihension
+#ls2 = [x for x in ls if 'a' in x] 
+#ls2 = [x.capitalize() for x in ls]
+#ls2 = [x[0].upper() + x[1:] for x in ls]
+
