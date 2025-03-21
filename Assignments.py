2c8183894e171388d979ff11968ea076b5c44623
Mavia1228 <mavia1228@gmail.com>
2025-03-21 11:59:45 +0500

    AssignmentsDone

---
+++ Assignments.py
@@ -0,0 +1,39 @@
+# Assignment 2
+# ls = ['laHOre','kaRAchi','mulTan','bahaWALpur','pisHAwar']
+# ls1 = []
+# for x in ls:
+#     x = x[0:1].upper() + x[1:-1].lower() + x[-1:].upper()
+#     ls1.insert(0,x)
+# ls = ls1
+# del ls1
+# print(ls)
+
+# Assignment 3
+#Question 1
+# num = 1
+# for x in range(2,22,2):
+#     print("2 * ", num , "= " , x)
+#     num = num +1
+
+#Question 2
+# ls = []
+# for x in range(2):
+#     ls.insert(0,int(input("Enter any number : ")))
+# print(ls)
+
+#Question 3
+# fac = int(input("Enter any number : "))
+# i = (fac-1)
+# while i > 0:
+#     fac = fac * i
+#     i = i-1
+# print(fac)
+
+#Question 4
+# count = 0
+# for x in range(100):
+#     num = str(x)
+#     for y in num:
+#         if y == '3':
+#             count = count + 1
+# print(count)
