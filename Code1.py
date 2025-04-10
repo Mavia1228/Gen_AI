880bd16cee87e6f19bed72a42b830159757ff85c
Mavia1228 <mavia1228@gmail.com>
2025-04-10 12:55:56 +0500

    Classes

--- Code1.py
+++ Code1.py
@@ -90,7 +90,6 @@
 # st1 = set(ls)
 # st2 = set(tp)
 # st3 = st1 | st2 # ls + tp and st1 union st2
-# #print(type(st3))
 # print("Union is " , st3)
 # st4 = st1 & st2 # st1 intersection st2
 # print("Intersection is " , st4)
@@ -111,12 +110,11 @@
 # dic.update({"Address" : "xyz","Alpha" : 1.99})
 # dic.pop("Address")
 # dic.popitem()
-# print(dic)
-# print(dic.items()gbn n,.d0)
+# print(dic.items())
 # print(dic.keys())
 # print(dic.values())
 
-# Pass  by value & Padd by refrence
+# Pass  by value & Pass by refrence
 # Non-Premitive data types(list,tuple,set,dictionaries) and Premitive data type
 
 # Dic Practic 1
@@ -153,18 +151,82 @@
 # try:
 #     print(y)
 # except:
-#     print("Error : Variale is not defined. Line 145")
+#     print("Error : Variale is not defined. Line 152")
 # finally:
 #     print("This is always executed. It means try or except is executed")
 
-def fun(city):
-    ls = ["Lahore","Sadiqabad","Multan","Karachi","Pishawar"]
-    # for x in ls:
-    #     if (city == x): print("City is Clean")
-    print([x for x in ls if city in x])
-    print(" Is Clean")
+# Function Practic 1
+# def fun(city):
+#     ls = ["Lahore","Sadiqabad","Multan","Karachi","Pishawar","Is Clean"]
+#     print([x for x in ls if city in x] + ls[-1:])    
+# fun(input("Enter Any City : "))
+
+# Function Practic 2
+
+# def fun(num):
+#     flag = False
+#     if (num <= 500 and num >= 50):
+#         flag = True
+#     print(flag)
+#     print(50+70-20*2/5)
+# fun(50+70-20*2/5)
+
+# Split method
+# st = "Hello my name is Mavia"
+# st = print(st.split("a"))
+
+# string formating
+# def sum(a,*b):
+#     print(f"hello world {a} : {b[0]} : {b[1]}")
+#     print(type(b))
+# sum(10,20,'Mavia')
+
+# Class
+# class sum:
+#     def setter(this,value1,value2):
+#         this.value1 = value1
+#         this.value2 = value2
+#     def getter(this):
+#         print(f"Value 1 is {this.value1} and value 2 is {this.value2}")
+#     def findSum(this):
+#         print("Sum is : " , this.value1 + this.value2)
+# s = sum()
+# s.setter(5,5)
+# s.getter()
+# s.findSum()
+
+class Employee:
+    def __init__(this,Name,Age):
+        this.Name = Name
+        this.Age = Age
+    def setter(this,Name,Age):
+        this.Name = Name
+        this.Age = Age
+    def getter(this):
+        print(f"{this.Name} : {this.Age}")
 
 
+e = Employee('Ameer Mavia','25')
+e1 = Employee('Shayaan','24')
+e2 = Employee('Ahmed','23')
+e3 = Employee('Gufraan','22')
+e4 = Employee('Raza','21')
 
-city = input("Enter Any City : ")
-fun(city)
+e.getter()
+e1.getter()
+e2.getter()
+e3.getter()
+e4.getter()
+
+# class sum:
+#     def setter(this,*values):
+#         this.value1 = values[0]
+#         this.value2 = values[1]
+#     def getter(this):
+#         print(f"Value 1 is {this.value1} and value 2 is {this.value2}")
+#     def findSum(this):
+#         print("Sum is : {this.value1 + this.value2}")
+# s = sum()
+# s.setter(5,10)
+# s.getter()
+# s.findSum()
